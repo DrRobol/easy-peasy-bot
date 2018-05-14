2e5adc6c9d2143584763b2a19b0dd940d46c85e9
Szymon Mikitiuk <szymon.mikitiuk@merapar.com>
2018-05-14 18:39:05 +0200

    Changed indentation; modified params in package.json; modified .gitignore

--- index.js
+++ index.js
@@ -9,16 +9,16 @@
  */
 
 function onInstallation(bot, installer) {
-    if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
-            if (err) {
-                console.log(err);
-            } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
-            }
-        });
-    }
+  if (installer) {
+    bot.startPrivateConversation({user: installer}, function (err, convo) {
+      if (err) {
+        console.log(err);
+      } else {
+        convo.say("I am a bot that has just joined your team");
+        convo.say("You must now /invite me to a channel so that I can be of use!");
+      }
+    });
+  }
 }
 
 
@@ -28,14 +28,14 @@ function onInstallation(bot, installer) {
 
 var config = {};
 if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
-    config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
-    };
+  var BotkitStorage = require("botkit-storage-mongo");
+  config = {
+    storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+  };
 } else {
-    config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
-    };
+  config = {
+    json_file_store: ((process.env.TOKEN)?"./db_slack_bot_ci/":"./db_slack_bot_a/"), //use a different name if an app or CI
+  };
 }
 
 /**
@@ -43,17 +43,17 @@ if (process.env.MONGOLAB_URI) {
  */
 
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
+  //Treat this as a custom integration
+  var customIntegration = require("./lib/custom_integrations");
+  var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+  var controller = customIntegration.configure(token, config, onInstallation);
 } else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+  //Treat this as an app
+  var app = require("./lib/apps");
+  var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
 } else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
+  console.log("Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment");
+  process.exit(1);
 }
 
 
@@ -66,13 +66,13 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
  * TODO: fixed b0rked reconnect behavior
  */
 // Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
-    console.log('** The RTM api just connected!');
+controller.on("rtm_open", function (bot) {
+  console.log("** The RTM api just connected!");
 });
 
-controller.on('rtm_close', function (bot) {
-    console.log('** The RTM api just closed');
-    // you may want to attempt to re-open
+controller.on("rtm_close", function (bot) {
+  console.log("** The RTM api just closed");
+  // you may want to attempt to re-open
 });
 
 
@@ -81,14 +81,25 @@ controller.on('rtm_close', function (bot) {
  */
 // BEGIN EDITING HERE!
 
-controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+controller.on("bot_channel_join", function (bot, message) {
+  bot.reply(message, "I'm here!");
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+controller.hears("hello", "direct_message", function (bot, message) {
+  bot.reply(message, "Hello!");
 });
 
+controller.hears("zryc", ["direct_mention", "mention"], function (bot, message) {
+  // bot.reply(message, "niedługo wyświetlę menu!");
+  var reply = fetch_menu();
+  bot.reply(message, reply);
+});
+
+function fetch_menu() {
+  // todo fetch data from https://jsonplaceholder.typicode.com/posts/7
+  // todo test fetching module in a separate file
+  return "menu fetched from the api";
+}
 
 /**
  * AN example of what could be:
